1ed227ecfbe3b7c797126b5f437d0b0237d87e05
khomess <sasha.khomenko12@gmail.com>
2022-11-12 20:13:24 +0200

    naming

--- src/main/java/lang/print/gaps/task2/advancedNamingConvention.java
+++ src/main/java/lang/print/gaps/task2/advancedNamingConvention.java
@@ -4,7 +4,7 @@ public class advancedNamingConvention {
     static final int adult_age = 18;
 
     int age = 10;
-    int  phoneHumber = 1384823;
+    int  phoneHumber = 1384813;
 
     void CallToFriend() {
         System.out.println(age);
